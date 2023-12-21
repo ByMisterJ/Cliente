f2ed685e54ad4a94660212b6a415ba8f040e959f
daw2 <josearanda9000@gmail.com>
2023-12-21 09:22:38 +0100

    commit del 06 arreglado y finalizado

--- 03/06.js
+++ 03/06.js
@@ -1,4 +1,10 @@
 function cambiarTema() {
-    document.getElementById('eltexto').style.color='red'
-    document.getElementById('eltexto').style.fontSize='120'
+    var texto =document.getElementById('eltexto')
+    if(texto.style.color=='red'){
+        texto.style.color='black'
+        texto.style.fontSize='16px'
+    }else{
+        texto.style.color='red'
+        texto.style.fontSize='120px'
+    }
 }
