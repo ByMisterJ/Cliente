b32fed19a30fe4af2b3045ee8107040a612c38d4
daw2 <josearanda9000@gmail.com>
2023-12-21 09:36:29 +0100

    commit del 07 finalizado

---
+++ 03/07.js
@@ -0,0 +1,32 @@
+function cambiarARojo() {
+    var texto =document.getElementById('otrotexto')
+    if(texto.style.color=='red'){
+        texto.style.color='black'
+        texto.style.fontSize='16px'
+    }else{
+        texto.style.color='red'
+        texto.style.fontSize='120px'
+    }
+}
+
+function cambiarAAzul() {
+    var texto =document.getElementById('otrotexto')
+    if(texto.style.color=='blue'){
+        texto.style.color='black'
+        texto.style.fontSize='16px'
+    }else{
+        texto.style.color='blue'
+        texto.style.fontSize='120px'
+    }
+}
+
+function cambiarAVerde() {
+    var texto =document.getElementById('otrotexto')
+    if(texto.style.color=='green'){
+        texto.style.color='black'
+        texto.style.fontSize='16px'
+    }else{
+        texto.style.color='green'
+        texto.style.fontSize='120px'
+    }
+}
