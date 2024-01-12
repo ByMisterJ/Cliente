b5bddc36c1483d979f20227e96e4e535bce11d44
daw2 <josearanda9000@gmail.com>
2024-01-12 11:45:11 +0100

    commit de eventos, evento01 finalizado, evento02 sin finalizar

--- 03/eventos/evento01.js
+++ 03/eventos/evento01.js
@@ -1,3 +1,11 @@
 function cambiarelemento(cab){
     document.getElementById(cab).innerHTML='Javascript en XHTML'
 }
+
+function entexto(cab){
+    document.getElementById(cab).style.fontSize='20px'
+}
+
+function fueratexto(cab){
+    document.getElementById(cab).style.fontSize='14px'
+}
